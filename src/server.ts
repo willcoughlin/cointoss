458403f062b8d02d71a3d78d05e5d9c56b54ed43
Will Coughlin <willcoughlin@gmail.com>
2019-06-14 18:43:32 -0500

    Create React app starter

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import path from 'path';
+
+const STATIC_DIR = path.join(__dirname, 'static');
 
 enum PortNumber {
   Http = 8080
 };
 
 const app = express();
+app.use(express.static(STATIC_DIR));
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.sendFile(path.join(STATIC_DIR, 'index.html'));
 });
 
 app.listen(PortNumber.Http, () => {
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/static/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist/static'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/, 
+        loader: "awesome-typescript-loader" 
+      },
+      { 
+        enforce: "pre", 
+        test: /\.js$/, 
+        loader: "source-map-loader" }
+    ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+}
